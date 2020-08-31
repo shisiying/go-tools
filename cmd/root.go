5afab07fe0be327582436502d5a0a0efb23a1f10
shisiying <shisiying@xhzyxed.cn>
2020-09-01 00:20:51 +0800

    feat:日期格式转换

--- cmd/root.go
+++ cmd/root.go
@@ -3,15 +3,16 @@ package cmd
 import "github.com/spf13/cobra"
 
 var rootCmd = &cobra.Command{
-	Use:"",
-	Short:"",
-	Long:"",
+	Use:   "",
+	Short: "",
+	Long:  "",
 }
 
 func Execute() error {
 	return rootCmd.Execute()
 }
 
-func init()  {
+func init() {
 	rootCmd.AddCommand(wordCmd)
+	rootCmd.AddCommand(timeCmd)
 }
